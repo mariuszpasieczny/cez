f13bcc1bc27b6cbb64b38500a5d153b5d897a5fd
mariuszpasieczny <mpasieczny@gmail.com>
2015-11-24 18:45:23 +0100

    1.3.84

--- application/modules/admin/models/Users/Table.php
+++ application/modules/admin/models/Users/Table.php
@@ -11,7 +11,6 @@ class Application_Model_Users_Table extends Application_Db_Table
     protected $_name = 'users';// table name
     protected $_primary = 'id'; // primary column name
     protected $_rowClass = 'Application_Model_Users_Row';
-    protected $_cacheInClass = true;
     
     protected $_referenceMap = array(
         'Role' => array(
@@ -20,11 +19,6 @@ class Application_Model_Users_Table extends Application_Db_Table
             'refColumns' => 'id'
         )
     );
-    
-    public function init($config = array()) {
-        $this->setCacheInClass(true);
-        $this->_setCache(Application_Db_Table::getDefaultCache());
-    }
     
     public function select() {
         if ($this->_lazyLoading === true) {

ddd6df18d274c8b2eae56599df2f6cb7d6fba13c
mariuszpasieczny <mpasieczny@gmail.com>
2015-12-29 20:06:34 +0100

    1.4.10

--- application/modules/admin/controllers/UsersController.php
+++ application/modules/admin/controllers/UsersController.php
@@ -178,12 +178,12 @@ class Admin_UsersController extends Application_Controller_Abstract
                 if (!$user->password) {
                     $user->repasshash = md5(time());
                     $user->save();
-                    $mail = new Zend_Mail();
+                    $mail = new Zend_Mail('UTF-8');
                     $mail->setFrom($this->_config->get(APPLICATION_ENV)->comments->mail->from);
                     $mail->addTo($user->email, "{$user->lastname} {$user->firstname}");
                     $mail->setSubject('Aktywacja konta w systemie CEZ Nplay');
                     $html = 'Witaj ' . $user->firstname . '<br><br>'
-                            . 'Przejdź na stronę <a href="http://www.' . $region . '.cez.nplay.pl/auth/change-password/hash/' . $user->repasshash . '">www.' . $region . '.cez.nplay.pl</a> by ustawić hasło.';
+                            . 'Przejdź na stronę <a href="http://www.' . $schema . '.cez.nplay.pl/auth/change-password/hash/' . $user->repasshash . '">www.' . $schema . '.cez.nplay.pl</a> by ustawić hasło.';
                     $mail->setBodyHtml($html);
                     $mail->send();
                 }

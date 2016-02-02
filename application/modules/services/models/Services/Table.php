7adaaddfe5859eacf873d7fed872ff30068bb224
mariuszpasieczny <mpasieczny@gmail.com>
2016-02-02 18:38:13 +0100

    1.4.33

--- application/modules/services/models/Services/Table.php
+++ application/modules/services/models/Services/Table.php
@@ -109,17 +109,20 @@ class Application_Model_Services_Table extends Application_Db_Table
         $fields[] = 'status';
         $fields[] = 'clientnumber';
         $fields[] = 'clientstreet';
+        $fields[] = 'clientcity';
         $fields[] = 'clientstreetno';
         $fields[] = 'clientapartment';
+        $fields[] = 'calendar';
+        $fields[] = 'servicetype';
         return $fields;
     }
     
     public function getAll($params = array(), $rows = null, $root = null) {
         if (!empty($params['planneddatefrom'])) {
-            $this->setWhere($this->getAdapter()->quoteInto("DATE_FORMAT(planneddate, '%Y-%m-%d') >= ?", $params['planneddatefrom']));
+            $this->setWhere($this->getAdapter()->quoteInto("DATE_FORMAT(CONCAT(planneddate, ' ', timefrom), '%Y-%m-%d %H:%M') >= ?", $params['planneddatefrom']));
         }
         if (!empty($params['planneddatetill'])) {
-            $this->setWhere($this->getAdapter()->quoteInto("DATE_FORMAT(planneddate, '%Y-%m-%d') <= ?", $params['planneddatetill']));
+            $this->setWhere($this->getAdapter()->quoteInto("DATE_FORMAT(CONCAT(planneddate, ' ', timefrom), '%Y-%m-%d %H:%M') <= ?", $params['planneddatetill']));
         }
         if (!empty($params['client'])) {
             $this->setWhere($this->getAdapter()->quoteInto("UPPER(client) LIKE UPPER(?)", "%{$params['client']}%"));

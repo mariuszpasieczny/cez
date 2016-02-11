6e5c9dafc7ed980be944ea29f52e63917f5e738a
mariuszpasieczny <mpasieczny@gmail.com>
2016-02-11 20:40:53 +0100

    2.1.16

--- data/db/sql/update_13.sql
+++ data/db/sql/update_13.sql
@@ -6,7 +6,7 @@ alter table servicesview ADD INDEX `id` (`id`);
 drop view servicecodesview;
 create table servicecodesview as 
 select s.*,'lublin' as instance from cez_dbtest.servicecodesview s;
-alter table servicecodesview ADD INDEX `id` (`id`);
+alter table servicecodesview ADD INDEX `id` (`id`,`number`,`planneddate`,`instance`);
 alter table servicecodesview ADD INDEX `serviceid` (`serviceid`);
 alter table servicecodesview ADD INDEX `attributeacronym` (`attributeacronym`);
 alter table servicecodesview ADD INDEX `codeacronym` (`codeacronym`);
